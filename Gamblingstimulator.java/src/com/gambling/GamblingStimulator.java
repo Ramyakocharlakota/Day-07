ee737a7a6771a730d2c1a4fb214f0fad70f78663
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2021-12-25 15:44:24 +0530

    ADDED

---
+++ Gamblingstimulator.java/src/com/gambling/GamblingStimulator.java
@@ -0,0 +1,14 @@
+package com.gambling;
+
+public class GamblingStimulator {
+
+	public static void main(String[] args) {
+		 int stake = 100;
+		   int bet = 1;
+		   System.out.println("Gambler put stake of $"+stake+" everyday.");
+		   System.out.println("Gambler bet $"+bet+" on everygame.");
+	   }
+	
+		// TODO Auto-generated method stub
+
+	}
