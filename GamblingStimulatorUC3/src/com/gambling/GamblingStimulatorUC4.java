75fd29e18020c87529836dd9da0d491a81818bf1
Ramya kocharlakota <Ramyakocharlakota@gmail.com>
2021-12-25 16:04:55 +0530

    ADDED

---
+++ GamblingStimulatorUC3/src/com/gambling/GamblingStimulatorUC4.java
@@ -0,0 +1,29 @@
+package com.gambling;
+
+public class GamblingStimulatorUC4 {
+		public static void Gambler() {
+			int stake = 100;
+			int bet = 1;
+			int numberOfBet = 0;
+			while(stake != 50 && stake != 150) {
+				double betCheck = Math.floor(Math.random() * 10) % 2;
+				if(betCheck == 1) {
+					stake += bet;
+					System.out.println("Gambler won by :" +stake);	
+				}else {
+					stake -= bet;
+					System.out.println("oops ! Gambler lost by :" +stake);
+				}
+				System.out.println("Value of stake is " +stake);
+			}
+			System.out.println("Gambler is resigning for the day");
+			numberOfBet = numberOfBet + 1;
+			System.out.println("He has bet " +numberOfBet+ " times." );	
+		}
+	 public static void main(String[] args) {
+		 Gambler();
+		// TODO Auto-generated method stub
+
+	}
+
+}
